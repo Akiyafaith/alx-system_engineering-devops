73bb4073713ae06877d2c9685c3cccd86e0ee8b5
akiyafaith <akiyafaith@gmail.com>
2023-07-24 07:59:22 -0700

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -23,8 +23,6 @@ if __name__ == '__main__':
     with open(csv_filename, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
         for task in todo_data:
             csv_writer.writerow([
                 user_data['id'],
