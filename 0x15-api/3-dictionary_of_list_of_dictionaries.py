71d46bf43d8422396ecbe2a1179228e4a6892423
akiyafaith <akiyafaith@gmail.com>
2023-07-24 10:21:13 -0700

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
 
     all_todo_data = {}
     for employee_id in range(1, 11):
-        user_data = requests.get(base_url + 'users/{}'.format(employee_id)).json()
+        user_data = requests.get(
+            base_url + 'users/{}'.format(employee_id)
+        ).json()
         todo_data = requests.get(
             base_url + 'todos',
             params={'userId': employee_id}
@@ -27,7 +29,7 @@ if __name__ == "__main__":
 
         all_todo_data[str(employee_id)] = tasks
     json_filename = 'todo_all_employees.json'
-     
+
     with open(json_filename, 'w') as json_file:
         json.dump(all_todo_data, json_file, indent=4)
     print("Data exported to {}".format(json_filename))
